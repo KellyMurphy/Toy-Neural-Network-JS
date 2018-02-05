7ac3b28ce8f2223e13a22cc1730fe4b881677683
Kelly M <kmurphy@frontedition.net>
2018-02-05 13:09:27 -0600

    Refactor DrawLoop and add Controls
    Refactor DrawLoop and add Controls

--- examples/xor/sketch.js
+++ examples/xor/sketch.js
@@ -14,24 +14,43 @@ let training_data = [{
 }];
 
 let lr_slider;
+let lr_para;
+let train_cb;
+let train_running;
 
 function setup() {
   createCanvas(400, 400);
-  nn = new NeuralNetwork(2, 2, 1);
+  nn = new NeuralNetwork(2, 4, 1);
   lr_slider = createSlider(0.01, 0.1, 0.05, 0.01);
+  //lr_para = createElement('p', '.1');
+  train_cb = createCheckbox("Training", 0);
+  train_cb.changed(trainingEnabled)
+  train_running=0
+
+}
+
+function trainingEnabled() {
+  train_running = this.checked();
 }
 
 function draw() {
-  background(0);
-
   nn.learning_rate = lr_slider.value();
+  if (train_running) {
+    train(5000);
+  }
+  updateCanvas();
+}
 
-  for (let i = 0; i < 5000; i++) {
+function train(n) {
+  for (let i = 0; i < n; i++) {
     let data = random(training_data);
     nn.train(data.inputs, data.targets);
   }
+}
 
-  let resolution = 20;
+function updateCanvas() {
+  background(0);
+  let resolution = 10;
   let cols = floor(width / resolution);
   let rows = floor(height / resolution);
 
@@ -48,7 +67,4 @@ function draw() {
       rect(x, y, resolution, resolution);
     }
   }
-
-
-
 }
